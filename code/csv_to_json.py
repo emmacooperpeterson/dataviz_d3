ade164bb6c943df17bddad2630ec59c2e8555053
Emma Peterson <emmacooperpeterson@gmail.com>
2017-11-08 14:18:27 -0600

    fixed output

--- code/csv_to_json.py
+++ code/csv_to_json.py
@@ -13,10 +13,13 @@ def csv_to_json(csv_file, json_file, columns):
     cols = columns
     reader = csv.DictReader(csvfile, cols)
 
+    json_list = []
+
     for i, row in enumerate(reader):
         if i > 0:
-            json.dump(row, jsonfile)
-            jsonfile.write('\n')
+            json_list.append(row)
+
+    json.dump(json_list, jsonfile)
 
 if __name__ == '__main__':
     csv_file = '../data/count_by_type.csv'
